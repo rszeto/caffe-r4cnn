217548a5f6474cf7e0afec20ee6e22be520bd6a8
Ryan Szeto <szetor@umich.edu>
2017-01-16 10:20:07 -0500

    Changed cmake-config to release mode, add Matlab build

--- config-cmake.mk
+++ config-cmake.mk
@@ -4,6 +4,7 @@ CAFFE_SOURCE_DIR:= /z/home/szetor/Documents/DENSO_VCA/RenderForCNN/caffe-r4cnn
 caffe:
 	module load cuda cudnn opencv gflags glog lmdb boost && \
 	cmake $(CAFFE_SOURCE_DIR) \
+			-DCMAKE_BUILD_TYPE=Release \
 			-DCMAKE_INSTALL_PREFIX=$(CAFFE_INSTALL_DIR) \
 			-DUSE_OPENCV=ON \
 			-DOpenCV_DIR=$${OPENCV_ROOT}/share/OpenCV \
@@ -14,4 +15,5 @@ caffe:
 			-DCUDA_ARCH_NAME=Manual \
 			-DCUDA_ARCH_BIN="20 21(20) 30 35 50 60 61(60)" \
 			-DCUDA_ARCH_PTX="60" \
-			-DALLOW_LMDB_NOLOCK=ON
+			-DALLOW_LMDB_NOLOCK=ON \
+			-DBUILD_matlab=ON
